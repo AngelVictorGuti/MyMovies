e9207d4e4410d9284793aceb8e6d9e3240964812
Angel <angel.victor@grupocmc.es>
2022-10-20 12:18:12 +0200

    Initial commit

---
+++ app/src/main/java/com/angelvictor/movies/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.angelvictor.movies
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
