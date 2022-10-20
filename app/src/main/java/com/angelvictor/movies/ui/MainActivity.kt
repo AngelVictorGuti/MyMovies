f76ebb1ed55968999bf98440bf255783c1b1fad0
Angel <angel.victor@grupocmc.es>
2022-10-20 14:45:43 +0200

    Add navigation to app

--- app/src/main/java/com/angelvictor/movies/ui/MainActivity.kt
+++ app/src/main/java/com/angelvictor/movies/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.angelvictor.movies
+package com.angelvictor.movies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.angelvictor.movies.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/angelvictor/movies/ui/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.angelvictor.movies.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel(
+
+) {
+
+}
